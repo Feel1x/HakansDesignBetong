a96d5512bd5ac04fc7a536ed19a7fc6e4159b179
Felix Broberg <165168078+Feel1x@users.noreply.github.com>
2024-06-11 02:29:28 +0200

    SHIT
    Bara blivit värre, inte lyckats fo header i separat fil, här endast problem med tiden, hinner inte ladda innan event

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,33 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+document.addEventListener("Load", function() {
 
+set
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".common-bar .top-bar .nav-menu");
+
+
+
+
+if (hamburger && navMenu) {
+    // Add event listener or perform other actions on header elements
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    })
+    
+    document.querySelectorAll(".nav-link").forEach(
+        n => n.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+    }))
+} else {
+    console.error("Hamburger or navMenu element not found.");
+}
+
+
+});
+
+
+/*
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
@@ -11,3 +38,4 @@ document.querySelectorAll(".nav-link").forEach(
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
 }))
+*/
