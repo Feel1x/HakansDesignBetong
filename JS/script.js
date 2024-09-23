dc215cee928b72d09ac1513fb4cbf6552fefc490
Feel1x <165168078+Feel1x@users.noreply.github.com>
2024-09-23 23:32:58 +0200

    Nya bilder o så

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,4 @@
-/*document.addEventListener("Load", function() {
 
-set
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".common-bar .top-bar .nav-menu");
-
-
-
-
-if (hamburger && navMenu) {
-    // Add event listener or perform other actions on header elements
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    })
-    
-    document.querySelectorAll(".nav-link").forEach(
-        n => n.addEventListener("click", () => {
-            hamburger.classList.remove("active");
-            navMenu.classList.remove("active");
-    }))
-} else {
-    console.error("Hamburger or navMenu element not found.");
-}
-
-
-});
-*/
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".common-bar .top-bar .nav-menu");
@@ -40,3 +13,5 @@ document.querySelectorAll(".nav-link").forEach(
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
 }))
+
+
